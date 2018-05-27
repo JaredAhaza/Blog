5dd13f6d7d00c662d862592f41e6fc290ae3540a
[JaredAhaza] <[jaredahazq@gmail.com]>
2018-05-27 11:19:59 +0300

    Created route for comments

--- app/main/views.py
+++ app/main/views.py
@@ -19,3 +19,17 @@ def blog():
         db.session.commit()
         return redirect(url_for('main.gallery'))
     return render_template('blog.html', test=test, blog_form=blog_form)
+
+@main.route('/comments/<int:id>', methods=['GET', 'POST'])
+@login_required
+def comments(id):
+    comment_form = CommentForm()
+    comments = Comments.query.order_by('-id').limit(3).all()
+    blog = Blog.query.get(id)
+    if comment_form.validate_on_submit():
+        comment = comment_form.comment.data
+        new_comment = Comments(comment=comment,users=current_user.username)
+        new_comment.save_comment()
+        return redirect(url_for('main.comments', id=blog.id))
+    return render_template('comments.html',blog=blog,comments=comments,comment_form=comment_form)
+
