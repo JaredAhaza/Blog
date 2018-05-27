9d103968ece085683c658d35712ce913984d83f3
[JaredAhaza] <[jaredahazq@gmail.com]>
2018-05-27 11:09:33 +0300

    created blog form

---
+++ app/main/forms.py
@@ -0,0 +1,11 @@
+from flask_wtf import FlaskForm
+from wtforms import TextAreaField, SubmitField, StringField, RadioField
+from wtforms.validators import Required
+
+class BlogForm(FlaskForm):
+    p_body = TextAreaField('Description', validators=[Required()])
+    p_author = StringField('Username', validators=[Required()])
+    category = RadioField('Pick Category',choices=[('people', 'people'),('nature', 'nature'),
+    ('artchitecture', 'artchitecture')],validators=[Required()])
+    p_url = TextAreaField('Enter image url', validators=[Required()])
+    submit = SubmitField('Submit')
