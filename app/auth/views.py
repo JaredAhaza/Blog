e3f7b6f64c185de1aef984ea3fe606e87dc85d73
[JaredAhaza] <[jaredahazq@gmail.com]>
2018-05-27 10:58:49 +0300

    created a sign in form

--- app/auth/views.py
+++ app/auth/views.py
@@ -4,8 +4,6 @@ from wtforms.validators import Required, Email, EqualTo
 from wtforms import ValidationError
 from ..models import User
 
-
-
 class SignUp(FlaskForm):
     email = StringField('Email', validators=[Required(), Email()])
     username = StringField('Your username', validators=[Required()])
@@ -22,3 +20,11 @@ class SignUp(FlaskForm):
     def validate_username(self, data_field):
         if User.query.filter_by(username=data_field.data).first():
             raise ValidationError('Username taken. Try another one')
+
+
+
+class SignIn(FlaskForm):
+    email = StringField('Email Address', validators=[Required(), Email()])
+    password = PasswordField('Password', validators=[Required()])
+    remember = BooleanField('Remember me')
+    submit = SubmitField('Sign In')
